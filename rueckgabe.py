ec24da5a0c90a5e9c06097d3582b8f6fee5d41e5
canneloni <46370119+canneloni@users.noreply.github.com>
2019-03-16 12:31:02 +0100

    Created GIT Project

---
+++ rueckgabe.py
@@ -0,0 +1,42 @@
+import csv
+
+# Die Bestellnummer des Kunden
+bestellNummer = input('Bitte gib deine Bestellnummer ein: ')
+
+
+# gruende = {'1. Falsches Produkt bestellt.': 1, '2. Falsche Größe bestellt.' : 2, '3. Es wurde ein Produkt zuviel geliefert.' :3, '4. Das Produkt gefällt mir nicht.' : 4, '5. Das Produkt ist defekt angekommen.': 5 }
+gruende = {1: '1. Falsches Produkt bestellt.',\
+           2: '2. Falsche Größe bestellt.',\
+           3: '3. Es wurde ein Produkt zuviel geliefert.',\
+           4: '4. Das Produkt gefällt mir nicht.',\
+           5: '5. Das Produkt ist defekt angekommen.'}
+
+# Ausgabe der Auswahlmöglichkeiten
+print('Es stehen folgende Rückgabegründe zur Auswahl:')
+for x in gruende:
+    print (gruende[x])
+
+def user_input():
+    grund_auswahl = int(input('Bitte gib deinen Rückgabegrund ein: '))
+    if  grund_auswahl <=5 and grund_auswahl>=1:
+        print('Ist dein Rückgabegrund: \n'+ gruende[(grund_auswahl)])
+        sicher=input('y=Ja, n=Nein \n')
+        if sicher=='y':
+            print('Deine Eingabe wurde gespeichert')
+        elif sicher=='n':
+            print('Bitte gib erneut deinen Rückgabegrund ein: ')
+            user_input()
+        else:
+            print('Bitte gib y für "Ja" und n für "nein" ein: \n')
+        # return grund_auswahl
+    else :
+        print('Bitte gib eine Zahl zwischen 1 und 5 als Rückgabegrund ein')
+        user_input()
+
+rueckgabeGrund = user_input()
+print(gruende[rueckgabeGrund])
+
+speichern = open('test.txt', 'a')
+speichern.write(str(rueckgabeGrund))
+
+
